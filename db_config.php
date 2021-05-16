5bc6701f3ff92a3dcfea9129a19fe8cf81358e8f
weizongli2 <weizongli2@gmail.com>
2021-05-16 21:26:59 +0800

    Update db_config.php

--- db_config.php
+++ db_config.php
@@ -1,8 +1,6 @@
 <?php
-        $servername="localhost";
-        $username="root";
-        $password="";
-        $mysql_database="ast20201db";
+        $servername=getenv("MYSQL_SERVICE_HOST");
+        $username=getenv("MYSQL_SERVICE_POST");
+        $password=getenv("databasepassword");
+        $mysql_database=getenv("databasename");
         ?>
-
-
